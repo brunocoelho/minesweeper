f49d7e251e205880f26b3a520c4e85a1e29dfa59
Bruno Coelho <bcs.cin@gmail.com>
2016-11-27 19:12:59 -0300

    Use ES6 methods instead of lodash

--- src/utils/matrix.js
+++ src/utils/matrix.js
@@ -1,10 +1,9 @@
-import _ from 'lodash';
 
 const getBombsNumber = (matrix, rowIndex, colIndex) => {
 	const positions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
 	let counter = 0;
 
-	_.forEach(positions, (position) => {
+	positions.forEach((position) => {
 		if (matrix[rowIndex + position[0]] && matrix[rowIndex + position[0]][colIndex + position[1]]) {
 			if (matrix[rowIndex + position[0]][colIndex + position[1]].value === 'BOMB') {
 				counter += 1;
@@ -22,8 +21,8 @@ const matrix = function (arr) {
 		matrix.push(arr.splice(0,16));
 	};
 
-	_.forEach(matrix, (row, rowIndex) => {
-		_.forEach(row, (field, colIndex) => {
+	matrix.forEach((row, rowIndex) => {
+		row.forEach((field, colIndex) => {
 			if (!matrix[rowIndex][colIndex]) {
 				matrix[rowIndex][colIndex] = {
 					value: getBombsNumber(matrix, rowIndex, colIndex),
