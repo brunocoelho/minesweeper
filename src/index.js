541e0b27d28e180a2db7d62ecf007c18852109c2
Bruno Coelho <bcs.cin@gmail.com>
2016-12-07 23:42:31 -0300

    Add router and About page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import About from './components/about';
 import App from './components/app';
 import './assets/css/index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="/about" component={About} />
+  </Router>
+), document.getElementById('root'));
